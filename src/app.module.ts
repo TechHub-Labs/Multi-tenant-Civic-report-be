3037f382b33d7ea5f6d47a321bbab65c8fa26197
shizzleclover <murewatajala@gmail.com>
2026-06-06 22:23:42 +0100

    Update decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
+import { TenantModule } from './features/tenant/tenant.module';
+import { TenantMiddleware } from './core/tenant/tenant.middleware';
 
 @Module({
   imports: [
@@ -11,8 +13,16 @@ import { DatabaseModule } from './core/database/database.module';
       envFilePath: '.env',
     }),
     DatabaseModule,
+    TenantModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(TenantMiddleware)
+      .exclude({ path: 'api/v1/tenants/(.*)/config', method: RequestMethod.GET })
+      .forRoutes('*');
+  }
+}
---
+++ src/core/tenant/tenant.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware, BadRequestException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { TenantService } from '../../features/tenant/tenant.service';
+
+@Injectable()
+export class TenantMiddleware implements NestMiddleware {
+  constructor(private readonly tenantService: TenantService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const slug = req.headers['x-tenant-slug'] as string;
+    
+    if (!slug) {
+      throw new BadRequestException('x-tenant-slug header is required');
+    }
+
+    // This will throw a NotFoundException if the tenant does not exist
+    const tenant = await this.tenantService.getTenantBySlug(slug);
+
+    // Attach tenant to the request object for downstream use
+    (req as any).tenant = tenant;
+    (req as any).tenantId = tenant._id;
+
+    next();
+  }
+}
