8e0853bfbf4bd2366808c3a974b8b5196731b97b
shizzleclover <murewatajala@gmail.com>
2026-06-07 13:54:08 +0100

    Render setup

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
 
   setupSwagger(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
